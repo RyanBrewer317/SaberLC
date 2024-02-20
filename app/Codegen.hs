af8ab3a1d50f4e8f8b2cce6fb34efa727ec54790
Ryan Brewer <ryanbrew317@gmail.com>
2024-02-19 16:40:58 -0800

    flesh out more Codegen.hs lowering

--- app/Codegen.hs
+++ app/Codegen.hs
@@ -11,6 +11,7 @@ import Data.Word (Word8)
 import Data.Map (Map, lookup, insert)
 import Prelude hiding (lookup)
 import Data.Void (absurd)
+import Data.Bits (shiftR)
 
 
 go :: [Stmt U] -> SLC [Word8]
@@ -89,8 +90,20 @@ opsToBytes (op:ops) = f op ++ opsToBytes ops
             ProjOp n -> [0x18,fromIntegral n]
             CallOp -> [0x19]
             PrintOp -> [0x1A]
-            LitOp n -> [0x1B,fromIntegral n,fromIntegral n,fromIntegral n,fromIntegral n] -- todo: spread across four bytes
-            GlobalFuncOp n -> [0x1C,fromIntegral n,fromIntegral n,fromIntegral n,fromIntegral n] -- todo: spread across four bytes
+            LitOp n -> 
+                [ 0x1B
+                , shiftR 24 n
+                , shiftR 16 n
+                , shiftR 8 n
+                , fromIntegral n
+                ]
+            GlobalFuncOp n -> 
+                [ 0x1C
+                , shiftR 24 n
+                , shiftR 16 n
+                , shiftR 8 n
+                , fromIntegral n
+                ]
             HaltOp n -> [0x1D,fromIntegral n]
             PackOp -> [0x1E]
             Word32Op -> [0x1F]
@@ -100,18 +113,38 @@ opsToBytes (op:ops) = f op ++ opsToBytes ops
             NewRgnOp -> [0x23]
             FreeRgnOp -> [0x24]
 
-goStmt :: Stmt U -> ReverseOps
-goStmt = undefined
+goStmt :: Stmt U -> (Id, ReverseOps)
+goStmt (Func x tvars params body) = undefined
 
 goExpr :: Int -> Int -> Map Id Int -> Map Id Int -> ExprCPS U U U -> ReverseOps
 goExpr rt_stack_size ct_stack_size rt_locals ct_locals e = case e of
-    AppCPS f targs args -> undefined
-    HaltCPS v -> let v_ops = goVal rt_stack_size ct_stack_size rt_locals ct_locals v in HaltOp 0:v_ops
-    LetCPS x t v scope -> undefined
-    TupleProjCPS x tpl i scope -> undefined
-    UnpackCPS () x y v scope -> undefined
-    MallocCPS () x ts scope -> undefined
-    InitCPS () x tpl i v scope -> undefined
+    AppCPS f targs args -> 
+        let (ct_stack_size2, targs_ops_rev) = foldr (\targ (i, ops_rev)-> (i+1, goType ct_stack_size ct_locals targ ++ ops_rev)) (ct_stack_size, []) targs in
+        let (rt_stack_size2, args_ops_rev) = foldr (\arg (i, ops_rev)-> (i+1, goVal (rt_stack_size+1) ct_stack_size2 rt_locals ct_locals arg ++ ops_rev)) (rt_stack_size, []) args in
+        let f_ops = goVal rt_stack_size2 ct_stack_size2 rt_locals ct_locals f in
+            CallOp:f_ops++args_ops_rev++targs_ops_rev
+    HaltCPS v -> HaltOp 0 : goVal rt_stack_size ct_stack_size rt_locals ct_locals v
+    LetCPS x _t v scope -> 
+        let v_ops = goVal rt_stack_size ct_stack_size rt_locals ct_locals v in
+        let scope_ops = goExpr (rt_stack_size+1) ct_stack_size (insert x (rt_stack_size+1) rt_locals) ct_locals scope in
+        scope_ops++v_ops
+    TupleProjCPS x tpl i scope -> 
+        let tpl_ops = goVal rt_stack_size ct_stack_size rt_locals ct_locals tpl in
+        let scope_ops = goExpr (rt_stack_size+1) ct_stack_size (insert x (rt_stack_size+1) rt_locals) ct_locals scope in
+        scope_ops++ProjOp i:tpl_ops
+    UnpackCPS () x _y v scope -> 
+        let v_ops = goVal rt_stack_size ct_stack_size rt_locals ct_locals v in
+        let scope_ops = goExpr (rt_stack_size+1) ct_stack_size (insert x (rt_stack_size+1) rt_locals) ct_locals scope in
+        scope_ops++UnpackOp:v_ops
+    MallocCPS () x ts scope -> 
+        let (ct_stack_size2, ts_ops) = foldr (\t (i, ops_rev)-> (i+1, goType i ct_locals t ++ ops_rev)) (ct_stack_size, []) ts in
+        let scope_ops = goExpr (rt_stack_size+1) ct_stack_size2 (insert x (rt_stack_size+1) rt_locals) ct_locals scope in
+        scope_ops++MallocOp:ts_ops
+    InitCPS () x tpl i v scope -> 
+        let tpl_ops = goVal rt_stack_size ct_stack_size rt_locals ct_locals tpl in
+        let v_ops = goVal (rt_stack_size+1) ct_stack_size rt_locals ct_locals v in
+        let scope_ops = goExpr (rt_stack_size+1) ct_stack_size (insert x (rt_stack_size+1) rt_locals) ct_locals scope in
+        scope_ops++InitOp i:v_ops++tpl_ops
 
 goVal :: Int -> Int -> Map Id Int -> Map Id Int -> ValCPS U U U -> ReverseOps
 goVal rt_stack_size ct_stack_size rt_locals ct_locals v = case v of
